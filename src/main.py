dc0bf914e804c8d3d57f8e2af507fce997286f90
jonathanissim <you@example.com>
2023-12-17 00:02:05 +0200

    Implementing util functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from tabulate import tabulate
 import marisa_trie
+from simple_colors import *
 
 
 def read_file_into_list(file_path):
@@ -11,15 +15,40 @@ def read_file_into_list(file_path):
     except FileNotFoundError:
         print(f"Error: File not found at {file_path}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while trying to open file: {e}")
 
     return word_list
 
 
+def filter_fixed_length_words(word_list: List[str], word_length=5):
+    return [word for word in word_list if len(word) == word_length]
+
+
+def print_crossword(words):
+    # print(cyan(tabulate(words), 'bright'))
+    print(cyan(tabulate(words), ['bright', 'reverse']))
+
+# algorithm
+# def find_crosswords(word_list, trie, word_length=5):
+#     potential_crossword = []
+#     remaining_words = word_list
+#     for level in range(word_length):
+#         potential_crossword.extend(remaining_words[0])
+#         remaining_words.pop(0)
+#
+
+# def is_legal_crossword(words):
+    
+
 def main():
-    word_list = read_file_into_list("../words.txt")
+    word_list = filter_fixed_length_words(read_file_into_list("../words.txt"))
+    word_list.sort()
+
+    print(f"number of words = {len(word_list)}")
     trie = marisa_trie.Trie(word_list)
-    print(trie.keys("ab"))
+    # print(trie.keys("ab"))
+
+    print_crossword(["chess", "crate", "snafu", "balls", "slime"])
 
 
 if __name__ == "__main__":
